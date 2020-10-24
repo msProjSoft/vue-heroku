56dc574cbe5b0e3ea10f2ba65c32606f886a1aa6
msProjSoft <ms.projsoft@gmail.com>
2020-10-24 22:55:43 +0100

    commit "v3"

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/login">Login</router-link> |
+      <router-link to="/login">LoginXX</router-link> |
+      <router-link to="/livros">Livros</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
